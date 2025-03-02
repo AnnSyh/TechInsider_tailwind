a1c0e1e6faf39b4dbc2feec57e7522255f699abe
AnnSyh <ann.syharevskaya@gmail.com>
2025-03-02 22:13:01 +0300

    first commit

---
+++ src/index.js
@@ -0,0 +1,11 @@
+function toggleDropdown() {
+	document.getElementById("myDropdown").classList.toggle("show");
+}
+function toggleSubmenu(nunber) {
+	document.getElementsByClassName("sidebar-menu__sublink")[nunber].classList.toggle("hidden");
+}
+function togglePopup() {
+	document.getElementById("myDropdown").classList.toggle("show");
+	document.getElementsByTagName("body")[0].classList.toggle("opened-modal");
+	document.getElementsByClassName("modal")[0].classList.toggle("hidden");
+}
---
+++ tailwind.config.js
@@ -0,0 +1,21 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+	content: [
+		"./src/**/*.{html,js}",
+		"./src/*.{html,js}",
+	],
+	theme: {
+		extend: {
+		  gridTemplateColumns: {
+			'24': 'repeat(24, minmax(0, 1fr))', // Определяем 24 колонки
+		  },
+		  gridColumn: {
+			'span-24': 'span 24 / span 24', // Добавляем кастомный класс col-span-18
+			'span-18': 'span 18 / span 18', // Добавляем кастомный класс col-span-18
+			'span-12': 'span 12 / span 12', // Добавляем кастомный класс col-span-9
+			'span-9': 'span 9 / span 9', // Добавляем кастомный класс col-span-9
+		  },
+		},
+	  },
+  	plugins: [],
+}
