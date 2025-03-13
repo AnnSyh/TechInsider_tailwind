2b6a8bc1d62e64b21aa15a1017796e0cd2459caf
AnnSyh <ann.syharevskaya@gmail.com>
2025-03-13 15:08:36 +0300

    add dropdown effe sticky blocks

--- src/index.js
+++ src/index.js
@@ -2,11 +2,14 @@
 function toggleDropdown() {
 	document.getElementById("myDropdown").classList.toggle("show");
 }
-function toggleSubmenu(nunber) {
-	document.getElementsByClassName("sidebar-menu__sublink")[nunber].classList.toggle("hidden");
+function toggleSubmenu(number) {
+	document.getElementsByClassName("sidebar-menu__sublink")[number].classList.toggle("hidden");
+	document.getElementsByClassName("sidebar-menu__arrow")[0].parentElement.parentElement.classList.toggle("my-[10px]");
+	const carrentMenuItem = document.getElementsByClassName("sidebar-menu__arrow")[number];
+	carrentMenuItem.querySelector("svg").classList.toggle("rotate-90");
 }
 function togglePopup() {
-	document.getElementById("myDropdown").classList.toggle("show");
+	// document.getElementById("myDropdown").classList.toggle("show");
 	document.getElementsByTagName("body")[0].classList.toggle("opened-modal");
 	document.getElementsByClassName("modal")[0].classList.toggle("hidden");
 }
